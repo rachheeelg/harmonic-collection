f266df37fd422056a0cf17775ef26cee2fd616f8
rachheeelg <rachelelizabeth2103@gmail.com>
2025-12-09 23:37:32 +0000

    final update yay

--- entry-10/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.carousel').forEach(function(carousel) {
-  new Flickity(carousel, {
-    cellAlign: 'left',
-    contain: true,
-    freeScroll: true,
-    prevNextButtons: false,
-    pageDots: false
-  });
-});
